0a1815403dca4c6fb0e7864d2909276373501f7b
1076051940@qq.com <1076051940@qq.com>
2019-05-16 22:05:26 +0800

    Site updated: 2019-05-16 22:04:52

--- scripts/click_show_text.js
+++ scripts/click_show_text.js
@@ -1,8 +1,19 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
+        console.log(e.target);
+        var color="#BAB9B5";
+        var fontWeight="bold";
+        
+       
         var a = new Array
         ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+
+        if($(e.target).hasClass("copy")){
+            a=new Array("复制成功");
+            color="red";
+            fontWeight="";
+        }
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -12,9 +23,8 @@ jQuery(document).ready(function($) {
             "top": y - 20,
             "left": x,
             "position": "absolute",
-            "font-weight": "bold",
-            "color": "#BAB9B5"
-        });
+            "font-weight": fontWeight,
+            "color": color});
         $("body").append($i);
         $i.animate({
             "top": y - 180,
